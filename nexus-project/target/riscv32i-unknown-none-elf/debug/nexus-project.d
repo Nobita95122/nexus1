15a273e677ff2462d24265261c393e05a23efabe
Nobita95122 <154524835+Nobita95122@users.noreply.github.com>
2024-06-25 12:39:19 +0000

    DFs

---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project.d
@@ -0,0 +1,1 @@
+/workspaces/nexus1/nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project: /workspaces/nexus1/nexus-project/src/main.rs
---
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
@@ -0,0 +1,1 @@
+/workspaces/nexus1/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspaces/nexus1/nexus-project/src/main.rs
